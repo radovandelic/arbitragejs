86985a1e2658ef8efcfea2e6af3cdb2a10ad2a70
Radovan Delic <infamouszgbgd@yahoo.com>
2017-10-18 18:36:37 +0200

    added clear button, currency display

--- app.js
+++ app.js
@@ -16,12 +16,12 @@ var logic = require("./logic");
 
 app.get("/", function (req, res) {
     var result = model.get();
-    res.render("index.ejs", { result: result });
+    res.render("index.ejs", { result: result, currency: "" });
 });
 
 app.get("/clear", function (req, res) {
     model.set(0);
-    res.render("index.ejs", { result: 0 })
+    res.render("index.ejs", { result: 0, currency: "" })
 });
 
 
@@ -30,13 +30,15 @@ app.post("/convert", function (req, res) {
     var number = model.get();
     var direction = req.body.direction ? true : false;
     var result = logic.convert(number, direction);
+    var currency = direction ? "$" : "€";
+    res.render("index.ejs", { result: result, currency: currency });
     res.render("index.ejs", { result: result });
     //res.send(req.body.number + ", direction: " + JSON.stringify(req.body.direction));
 });
 
 app.get("/convert", function (req, res) {
     var result = model.get();
-    res.render("index.ejs", { result: result });
+    res.render("index.ejs", { result: result, currency: "" });
 });
 
 app.listen(port, function (req, res) {
