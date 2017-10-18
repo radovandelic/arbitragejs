38d47eb4fede4d50d5033a065aac5a83944b75d8
Radovan Delic <infamouszgbgd@yahoo.com>
2017-10-18 18:25:25 +0200

    working first version

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
 var express = require("express");
-var app = express ();
-var model = require("./model");
-var logic = require("./logic");
+var app = express();
 var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use("/", express.static(__dirname + "/public"));
 var port = 3000;
 
-app.set ("view engine", "ejs");
+app.set("view engine", "ejs");
 
-
+var model = require("./model");
+var logic = require("./logic");
 //var routes = require("./routes");
 //app.use(routes)
 
-app.get("/",function(req,res){
-    res.send("home");
+app.get("/", function (req, res) {
+    var result = model.get();
+    res.render("index.ejs", { result: result });
 });
 
-app.get("/clear",function(req,res){
+app.get("/clear", function (req, res) {
     model.set(0);
-    res.send("clear function");
+    res.render("index.ejs", { result: 0 })
 });
 
 
-app.post("/convert",function(req,res){
+app.post("/convert", function (req, res) {
     model.set(req.body.number);
     var number = model.get();
-    var direction = req.body.direction;
-    var result = logic.convert(number, direction)
-    res.send(result);
+    var direction = req.body.direction ? true : false;
+    var result = logic.convert(number, direction);
+    res.render("index.ejs", { result: result });
+    //res.send(req.body.number + ", direction: " + JSON.stringify(req.body.direction));
 });
 
-app.listen(port,function(req,res){
+app.get("/convert", function (req, res) {
+    var result = model.get();
+    res.render("index.ejs", { result: result });
+});
+
+app.listen(port, function (req, res) {
     console.log("Server started!, listening on port " + port);
 });
